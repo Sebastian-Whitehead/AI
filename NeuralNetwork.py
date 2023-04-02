6ed09307a395e997a2097e3971306b297bca67b0
Tonko <72245722+tbosse20@users.noreply.github.com>
2023-04-02 18:25:42 +0200

    Comment NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,29 +5,30 @@ class NeuralNetwork:
     structure = [2, 3, 2]
 
     def __init__(self):
-        self.construct()
+        self.construct() # Construct weights and baises 
 
     def construct(self):
-        self.layers = list()
+        self.layers = list() # Declare layers list
 
-        for i, startNeurons in enumerate(self.structure):
-            layer = list()
-            if i >= len(self.structure) - 1: break
-            neurons = self.structure[i + 1]
-            weights = np.random.random((neurons, startNeurons))
+
+        for i, startNeurons in enumerate(self.structure): # Loop structure
+            layer = list() # Declare layer list
+            if i >= len(self.structure) - 1: break # Break last iteration
+            neurons = self.structure[i + 1] # End neurons size
+            weights = np.random.random((neurons, startNeurons)) # Make random weights matrix
             layer.append(weights)
-            bias = np.random.random((neurons, 1))
+            bias = np.random.random((neurons, 1)) # Make random biases list
             layer.append(bias)
             self.layers.append(layer)
 
     def calculateNetwork(self, inputValues):
-        layerValues = inputValues
-        for layer in self.layers:
-            weights, bias = layer[0], layer[1]
+        layerValues = inputValues # Initialize layerValues to input
+        for layer in self.layers: # Loop each layer
+            weights, bias = layer[0], layer[1] # Initialize weights and biases
             layerValues = self.calculateLayer(layerValues, weights, bias)
-        return layerValues
+        return layerValues # Return final layer
 
-    def calculateLayer(self, inputValues, weights, bias):
+    def calculateLayer(self, inputValues, weights, bias): # Calculate layer (x*w+b=y)
         layer = np.multiply(inputValues, weights)
         layer = np.add(layer, bias)
         layer = np.sum(layer, axis=1)
